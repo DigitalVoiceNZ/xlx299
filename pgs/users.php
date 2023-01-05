67f9bb86a3f80ab20579ea5868981d884fa80d3c
Jim Tittsler ZL2IA <jimt@onjapan.net>
2023-01-06 03:42:46 +1300

    users page wrapping columns of module users
    show module icon above possibly wrapped text description
    add Brandmeister logo
    improve module documentation

--- pgs/users.php
+++ pgs/users.php
@@ -97,145 +97,142 @@ if (isset($_GET['do'])) {
 ?>
 
 <div class="container xusers">
-<div class="row">
-   <div class="table-responsive">
-      <table class="table table-sm table-striped-custom table-hover">
-<?php
-if ($PageOptions['UserPage']['ShowFilter']) {
-  echo '
- <tr>
-   <th colspan="8">
-      <table width="100%" border="0">
-         <tr>
-            <td align="left">
-               <form name="frmFilterCallSign" method="post" action="./index.php">
-                  <input type="hidden" name="do" value="SetFilter" />
-                  <input type="text" class="FilterField" value="'.$_SESSION['FilterCallSign'].'" name="txtSetCallsignFilter" placeholder="Callsign" onfocus="SuspendPageRefresh();" onblur="setTimeout(ReloadPage, '.$PageOptions['PageRefreshDelay'].');" />
-                  <input type="submit" value="Apply" class="FilterSubmit" />
-               </form>
-            </td>';
-              if (($_SESSION['FilterModule'] != null) || ($_SESSION['FilterCallSign'] != null)) {
-                  echo '
-         <td><a href="./index.php?do=resetfilter" class="smalllink">Disable filters</a></td>';
+  <div class="row">
+     <div class="table-responsive">
+        <table class="table table-sm table-striped-custom table-hover">
+  <?php
+  if ($PageOptions['UserPage']['ShowFilter']) {
+    echo '
+   <tr>
+     <th colspan="8">
+        <table width="100%" border="0">
+           <tr>
+              <td align="left">
+                 <form name="frmFilterCallSign" method="post" action="./index.php">
+                    <input type="hidden" name="do" value="SetFilter" />
+                    <input type="text" class="FilterField" value="'.$_SESSION['FilterCallSign'].'" name="txtSetCallsignFilter" placeholder="Callsign" onfocus="SuspendPageRefresh();" onblur="setTimeout(ReloadPage, '.$PageOptions['PageRefreshDelay'].');" />
+                    <input type="submit" value="Apply" class="FilterSubmit" />
+                 </form>
+              </td>';
+                if (($_SESSION['FilterModule'] != null) || ($_SESSION['FilterCallSign'] != null)) {
+                    echo '
+           <td><a href="./index.php?do=resetfilter" class="smalllink">Disable filters</a></td>';
+                }
+                echo '            
+              <td align="right" style="padding-right:3px;">
+                 <form name="frmFilterModule" method="post" action="./index.php">
+                    <input type="hidden" name="do" value="SetFilter" />
+                    <input type="text" class="FilterField" value="'.$_SESSION['FilterModule'].'" name="txtSetModuleFilter" placeholder="Module" onfocus="SuspendPageRefresh();" onblur="setTimeout(ReloadPage, '.$PageOptions['PageRefreshDelay'].');" />
+                    <input type="submit" value="Apply" class="FilterSubmit" />
+                 </form>
+              </td>
+        </table>
+     </th>
+  </tr>';
+  }
+  ?>
+  	 <thead>
+               <tr class="table-center">   
+                   <th scope="col">#</th>
+                   <th scope="col">Flag</th>
+                   <th scope="col">Callsign</th>
+                   <th scope="col">Suffix</th>
+                   <th scope="col">DPRS</th>
+                   <th scope="col">Via / Peer</th>
+                   <th scope="col">Last heard</th>
+                   <th scope="col">Module</th>
+  	      </tr>
+          </thead>
+  <?php
+  
+  $Reflector->LoadFlags();
+  for ($i=0;$i<$Reflector->StationCount();$i++) {
+      // if PNUT user and more recent than cache and not cached
+      if (($Reflector->Stations[$i]->GetSuffix() == 'PNUT')
+          && array_key_exists($Reflector->Stations[$i]->GetModule(), $pnutrooms)
+          && (time() - $Reflector->Stations[$i]->GetLastHeardTime() < PNUTREFRESH)
+          && (!inCache($Reflector->Stations[$i]->GetCallsignOnly(), $pnutrooms[$Reflector->Stations[$i]->GetModule()]))
+          && !apcu_exists('PNUTAPILOCK')) {
+          apcu_delete('PNUTCACHEVALID');
+      }
+      $ShowThisStation = true;
+      if ($PageOptions['UserPage']['ShowFilter']) {
+          $CS = true;
+          if ($_SESSION['FilterCallSign'] != null) {
+              if (!fnmatch($_SESSION['FilterCallSign'], $Reflector->Stations[$i]->GetCallSign(), FNM_CASEFOLD)) {
+                  $CS = false;
               }
-              echo '            
-            <td align="right" style="padding-right:3px;">
-               <form name="frmFilterModule" method="post" action="./index.php">
-                  <input type="hidden" name="do" value="SetFilter" />
-                  <input type="text" class="FilterField" value="'.$_SESSION['FilterModule'].'" name="txtSetModuleFilter" placeholder="Module" onfocus="SuspendPageRefresh();" onblur="setTimeout(ReloadPage, '.$PageOptions['PageRefreshDelay'].');" />
-                  <input type="submit" value="Apply" class="FilterSubmit" />
-               </form>
-            </td>
-      </table>
-   </th>
-</tr>';
-}
-?>
-	 <thead>
-             <tr class="table-center">   
-                 <th scope="col">#</th>
-                 <th scope="col">Flag</th>
-                 <th scope="col">Callsign</th>
-                 <th scope="col">Suffix</th>
-                 <th scope="col">DPRS</th>
-                 <th scope="col">Via / Peer</th>
-                 <th scope="col">Last heard</th>
-                 <th scope="col">Module</th>
-	      </tr>
-        </thead>
-<?php
-
-$Reflector->LoadFlags();
-for ($i=0;$i<$Reflector->StationCount();$i++) {
-    // if PNUT user and more recent than cache and not cached
-    if (($Reflector->Stations[$i]->GetSuffix() == 'PNUT')
-        && array_key_exists($Reflector->Stations[$i]->GetModule(), $pnutrooms)
-        && (time() - $Reflector->Stations[$i]->GetLastHeardTime() < PNUTREFRESH)
-        && (!inCache($Reflector->Stations[$i]->GetCallsignOnly(), $pnutrooms[$Reflector->Stations[$i]->GetModule()]))
-        && !apcu_exists('PNUTAPILOCK')) {
-        apcu_delete('PNUTCACHEVALID');
-    }
-    $ShowThisStation = true;
-    if ($PageOptions['UserPage']['ShowFilter']) {
-        $CS = true;
-        if ($_SESSION['FilterCallSign'] != null) {
-            if (!fnmatch($_SESSION['FilterCallSign'], $Reflector->Stations[$i]->GetCallSign(), FNM_CASEFOLD)) {
-                $CS = false;
-            }
-        }
-        $MO = true;
-        if ($_SESSION['FilterModule'] != null) {
-            if (trim(strtolower($_SESSION['FilterModule'])) != strtolower($Reflector->Stations[$i]->GetModule())) {
-                $MO = false;
-            }
-        }
-
-        $ShowThisStation = ($CS && $MO);
-    }
-
-    if ($ShowThisStation) {
-
-        echo '
-      <tr class="table-center">
-       <td>';
-        if ($i == 0 && $Reflector->Stations[$i]->GetLastHeardTime() > (time() - 60)) {
-            echo '<img src="./img/tx.gif" style="margin-top:3px;" height="20"/>';
-        } else {
-            echo($i + 1);
-        }
-
-
-        echo '</td>
-        <td>';
-
-        list ($Flag, $Name) = $Reflector->GetFlag($Reflector->Stations[$i]->GetCallSign());
-        if (file_exists("./img/flags/" . $Flag . ".png")) {
-            echo '<a href="#" class="tip"><img src="./img/flags/' . $Flag . '.png" class="table-flag" alt="' . $Name . '"><span>' . $Name . '</span></a>';
-        }
-        echo '</td>
-   <td><a href="https://www.qrz.com/db/' . $Reflector->Stations[$i]->GetCallsignOnly() . '" class="pl" target="_blank">' . $Reflector->Stations[$i]->GetCallsignOnly() . '</a></td>
-   <td>' . $Reflector->Stations[$i]->GetSuffix() . '</td>
-   <td><a href="http://www.aprs.fi/' . $Reflector->Stations[$i]->GetCallsignOnly() . '" class="pl" target="_blank"><i class="material-icons md-48">satellite</i></a></td>
-   <td>' . $Reflector->Stations[$i]->GetVia();
-        if ($Reflector->Stations[$i]->GetPeer() != $Reflector->GetReflectorName()) {
-            echo ' / ' . $Reflector->Stations[$i]->GetPeer();
-        }
-        echo '</td>
-   <td>' . @date("d.m.Y H:i", $Reflector->Stations[$i]->GetLastHeardTime()) . '</td>
-   <td>' . $Reflector->Stations[$i]->GetModule() . '</td>
- </tr>';
-    }
-    if ($i == $PageOptions['LastHeardPage']['LimitTo']) {
-        $i = $Reflector->StationCount() + 1;
-    }
-}
-
-?> 
- 
-      </table>
-   </div>
-</div>
+          }
+          $MO = true;
+          if ($_SESSION['FilterModule'] != null) {
+              if (trim(strtolower($_SESSION['FilterModule'])) != strtolower($Reflector->Stations[$i]->GetModule())) {
+                  $MO = false;
+              }
+          }
+  
+          $ShowThisStation = ($CS && $MO);
+      }
+  
+      if ($ShowThisStation) {
+  
+          echo '
+        <tr class="table-center">
+         <td>';
+          if ($i == 0 && $Reflector->Stations[$i]->GetLastHeardTime() > (time() - 60)) {
+              echo '<img src="./img/tx.gif" style="margin-top:3px;" height="20"/>';
+          } else {
+              echo($i + 1);
+          }
+  
+  
+          echo '</td>
+          <td>';
+  
+          list ($Flag, $Name) = $Reflector->GetFlag($Reflector->Stations[$i]->GetCallSign());
+          if (file_exists("./img/flags/" . $Flag . ".png")) {
+              echo '<a href="#" class="tip"><img src="./img/flags/' . $Flag . '.png" class="table-flag" alt="' . $Name . '"><span>' . $Name . '</span></a>';
+          }
+          echo '</td>
+     <td><a href="https://www.qrz.com/db/' . $Reflector->Stations[$i]->GetCallsignOnly() . '" class="pl" target="_blank">' . $Reflector->Stations[$i]->GetCallsignOnly() . '</a></td>
+     <td>' . $Reflector->Stations[$i]->GetSuffix() . '</td>
+     <td><a href="http://www.aprs.fi/' . $Reflector->Stations[$i]->GetCallsignOnly() . '" class="pl" target="_blank"><i class="material-icons md-48">satellite</i></a></td>
+     <td>' . $Reflector->Stations[$i]->GetVia();
+          if ($Reflector->Stations[$i]->GetPeer() != $Reflector->GetReflectorName()) {
+              echo ' / ' . $Reflector->Stations[$i]->GetPeer();
+          }
+          echo '</td>
+     <td>' . @date("d.m.Y H:i", $Reflector->Stations[$i]->GetLastHeardTime()) . '</td>
+     <td>' . $Reflector->Stations[$i]->GetModule() . '</td>
+   </tr>';
+      }
+      if ($i == $PageOptions['LastHeardPage']['LimitTo']) {
+          $i = $Reflector->StationCount() + 1;
+      }
+  }
+  
+  ?> 
+   
+        </table>
+     </div>
+  </div>
 </div>
 <?php
 	echo <<<EOD
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark navbar-fixed-top">
-    <div class="container">
-	<p>&nbsp;</p>
-    </div>
+  <div>
+    <p>&nbsp;</p>
+  </div>
 </nav>
-<div style="margin: 0 2em">
-<div class="row">
-   <div class="table-responsive">
-      <table class="table table-sm table-striped-custom table-hover moduleusers">
+<div class="container">
+  <div class="row">
 EOD;
 
 $Modules = $Reflector->GetModules();
 sort($Modules, SORT_STRING);
-echo '<tr>';
 for ($i=0;$i<count($Modules);$i++) {
    
-   if ($Modules[$i] > 'N') break;
-
+   echo '<div class="col-xl-2 col-lg-3 col-md-4 col-sm-6 py-5">';
+   echo '  <div style="height: 150px;">';
    if (isset($PageOptions['ModuleIcons'][$Modules[$i]]))
    {
       $desc = "";
@@ -243,39 +240,24 @@ for ($i=0;$i<count($Modules);$i++) {
         $desc = $PageOptions['ModuleNames'][$Modules[$i]];
         $iconfile = $PageOptions['ModuleIcons'][$Modules[$i]];
         $iconclass = (strpos($iconfile, '-130') !== false) ? 'ModuleIconW' : 'ModuleIcon';
-      echo '<th>'.$Modules[$i].'<br/>'.$desc.'<br/><img class="'.$iconclass.'" alt="icon" src="'.$PageOptions['ModuleIcons'][$Modules[$i]].'"></th>';
+      echo '<b>'.$Modules[$i].'</b><br/><img class="'.$iconclass.'" alt="icon" src="'.$PageOptions['ModuleIcons'][$Modules[$i]].'"><br/><b>'.$desc.'</b>';
    }
    else if (isset($PageOptions['ModuleNames'][$Modules[$i]])) 
    {
-      echo '<th>'.$Modules[$i].'<br>'.$PageOptions['ModuleNames'][$Modules[$i]].'</th>';
-       //if (trim($PageOptions['ModuleNames'][$Modules[$i]]) != "") {
-       //    echo '<br />';
-       //}
-      //echo .'</th>';
+      echo $Modules[$i].'<br>'.$PageOptions['ModuleNames'][$Modules[$i]];
    }
    else {
-   echo '
-  
-      <th>'.$Modules[$i].'</th>';
+      echo $Modules[$i];
    }
-}
-
-echo '</tr><tr>';
-
-$GlobalPositions = array();
-
-for ($i=0;$i<count($Modules);$i++) {
-    
-   if ($Modules[$i] > 'N') break;
-
+   echo '  </div>'; 
    $Users = $Reflector->GetNodesInModulesByID($Modules[$i]);
-   echo '<td><table class="table table-hover">';
+   echo '<table class="table table-hover">';
 
    $UserCheckedArray = array();
    
    for ($j=0;$j<count($Users);$j++) {
        [$Displayname, $protocol] = $Reflector->GetCallsignSuffixAndProtocolByID($Users[$j]);
-       $protocol = str_replace('DMRMmdvm', 'MMDVM', $protocol);
+       $protocol = str_replace('DMRMmdvm', 'DMR', $protocol);
       echo '
             <tr>
                <td><a href="http://www.aprs.fi/'.$Displayname.'" class="pl" target="_blank">'.$Displayname.'</a> <sup>'.$protocol.'</sup> </td>
@@ -303,107 +285,11 @@ for ($i=0;$i<count($Modules);$i++) {
                 </tr>';
       }
    }
-   echo '</table></td>';
+   echo '</table></div>';
 }
 
 echo <<<EOD
-        </tr>
-      </table>
-   </div>
-</div>
-</div>
-EOD;
-	echo <<<EOD
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark navbar-fixed-top">
-    <div class="container">
-	<p>&nbsp;</p>
     </div>
-</nav>
-<div style="margin: 0 2em">
-<div class="row">
-   <div class="table-responsive">
-      <table class="table table-sm table-striped-custom table-hover moduleusers">
-EOD;
-
-$Modules = $Reflector->GetModules();
-sort($Modules, SORT_STRING);
-echo '<tr>';
-for ($i=0;$i<count($Modules);$i++) {
-   
-   if ($Modules[$i] <= 'N') continue;
-   if (isset($PageOptions['ModuleIcons'][$Modules[$i]]))
-   {
-      $desc = "";
-      if (isset($PageOptions['ModuleNames'][$Modules[$i]]))
-        $desc = $PageOptions['ModuleNames'][$Modules[$i]];
-        $iconfile = $PageOptions['ModuleIcons'][$Modules[$i]];
-        $iconclass = (strpos($iconfile, '-130') !== false) ? 'ModuleIconW' : 'ModuleIcon';
-      echo '<th>'.$Modules[$i].'<br/>'.$desc.'<br/><img class="'.$iconclass.'" alt="icon" src="'.$PageOptions['ModuleIcons'][$Modules[$i]].'"></th>';
-   }
-   else if (isset($PageOptions['ModuleNames'][$Modules[$i]])) 
-   {
-      echo '<th>'.$Modules[$i].'<br>'.$PageOptions['ModuleNames'][$Modules[$i]].'</th>';
-       //if (trim($PageOptions['ModuleNames'][$Modules[$i]]) != "") {
-       //    echo '<br />';
-       //}
-      //echo .'</th>';
-   }
-   else {
-   echo '
-  
-      <th>'.$Modules[$i].'</th>';
-   }
-}
-
-echo '</tr><tr>';
-
-$GlobalPositions = array();
-
-for ($i=0;$i<count($Modules);$i++) {
-    
-   if ($Modules[$i] <= 'N') continue;
-   $Users = $Reflector->GetNodesInModulesByID($Modules[$i]);
-   echo '<td><table class="table table-hover">';
-
-   $UserCheckedArray = array();
-   
-   for ($j=0;$j<count($Users);$j++) {
-       [$Displayname, $protocol] = $Reflector->GetCallsignSuffixAndProtocolByID($Users[$j]);
-       $protocol = str_replace('DMRMmdvm', 'MMDVM', $protocol);
-      echo '
-            <tr>
-               <td><a href="http://www.aprs.fi/'.$Displayname.'" class="pl" target="_blank">'.$Displayname.'</a> <sup>'.$protocol.'</sup> </td>
-            </tr>';
-      $UserCheckedArray[] = $Users[$j];
-   }
-   // add Peanut users on this module
-   $thismodule = array_key_exists($Modules[$i], $pnutrooms) ? $pnutrooms[$Modules[$i]] : '---';
-   foreach ($pnut as $pu) {
-       if ($pu->room == $thismodule) {
-           $call = $Displayname = $pu->Call;
-           switch (strtolower($pu->device)) {
-              case 'android':
-                  $Displayname .= '&nbsp;<i class="material-icons">android</i>';
-                  break;
-              case 'windows':
-                  $Displayname .= '&nbsp;<i class="material-icons small">laptop_windows</i>';
-                  break;
-	      default:
-                  $Displayname .= '-P';
-          }
-          echo '
-                <tr>
-                   <td><a href="https://aprs.fi/'.$call.'" class="pl" target="_blank">'.$Displayname.'</a> </td>
-                </tr>';
-      }
-   }
-   echo '</table></td>';
-}
-
-echo <<<EOD
-        </tr>
-      </table>
-   </div>
-</div>
+  </div>
 </div>
 EOD;
